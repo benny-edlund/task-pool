22b9c32643544a72f55f7fe2f1e779dcd2e276dc
Benny Edlund <benny.edlund@unity3d.com>
2023-01-08 14:30:12 +1300

    Removing FMT dependency and disabling fuzz tests until there is something to test

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -1,22 +1,22 @@
-#include <fmt/format.h>
+#include <iostream>
 #include <iterator>
 #include <utility>
 
 auto sum_values(const uint8_t *Data, size_t Size)
 {
-  constexpr auto scale = 1000;
+    constexpr auto scale = 1000;
 
-  int value = 0;
-  for (std::size_t offset = 0; offset < Size; ++offset) {
-    value += static_cast<int>(*std::next(Data, static_cast<long>(offset))) * scale;
-  }
-  return value;
+    int value = 0;
+    for (std::size_t offset = 0; offset < Size; ++offset) {
+        value += static_cast<int>(*std::next(Data, static_cast<long>(offset))) * scale;
+    }
+    return value;
 }
 
 // Fuzzer that attempts to invoke undefined behavior for signed integer overflow
 // cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-  fmt::print("Value sum: {}, len{}\n", sum_values(Data, Size), Size);
-  return 0;
+    std::cerr << "Value sum: " << sum_values(Data, Size) << ", len " << Size << '\n';
+    return 0;
 }
