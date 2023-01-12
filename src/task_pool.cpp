5855e707a8707d22a8108c8c5893a3cf1de1c282
benny-edlund <benny.edlund@gmail.com>
2023-01-12 13:02:38 +1300

    Inital implementation of future linking

--- src/task_pool.cpp
+++ src/task_pool.cpp
@@ -3,6 +3,7 @@
 #include <condition_variable>
 #include <exception>
 #include <functional>
+#include <iterator>
 #include <mutex>
 #include <queue>
 #include <thread>
@@ -12,71 +13,95 @@ namespace be {
 
 struct task_pool::Impl
 {
-    mutable std::mutex tasks_mutex_ = {};
-    std::queue<task_proxy> tasks_ = {};
-    std::condition_variable task_added_ = {};
-    std::atomic<bool> waiting_{ false };
-    std::atomic<bool> paused_{ false };
-    std::atomic<bool> abort_{ false };
-    std::condition_variable task_completed_ = {};
-    std::atomic<std::size_t> tasks_queued_{ 0 };
-    unsigned thread_count_ = 0;
-    std::unique_ptr<std::thread[]> threads_;//  NOLINT (c-arrays)
+    mutable std::mutex               tasks_mutex_       = {};
+    std::queue< task_proxy >         tasks_             = {};
+    mutable std::mutex               check_tasks_mutex_ = {};
+    std::vector< task_proxy >        tasks_to_check_    = {};
+    std::condition_variable          task_added_        = {};
+    std::atomic< bool >              waiting_{ false };
+    std::atomic< bool >              paused_{ false };
+    std::atomic< bool >              abort_{ false };
+    std::condition_variable          task_completed_ = {};
+    std::atomic< std::size_t >       tasks_queued_{ 0 };
+    unsigned                         thread_count_ = 0;
+    std::unique_ptr< std::thread[] > threads_; //  NOLINT (c-arrays)
 
-    explicit Impl(unsigned thread_count)
-        : thread_count_(determine_thread_count(thread_count)),
-          threads_(std::make_unique<std::thread[]>(thread_count))// NOLINT (c-arrays)
+    explicit Impl( unsigned thread_count )
+        : thread_count_( determine_thread_count( thread_count ) )
+        , threads_( std::make_unique< std::thread[] >( thread_count ) ) // NOLINT (c-arrays)
     {
         create_threads();
     }
     ~Impl() { destroy_threads(); }
 
-    Impl(Impl const &) = delete;
-    Impl(Impl &&) = delete;
-    Impl &operator=(Impl const &) = delete;
-    Impl &operator=(Impl &&) = delete;
+    Impl( Impl const& )            = delete;
+    Impl( Impl&& )                 = delete;
+    Impl& operator=( Impl const& ) = delete;
+    Impl& operator=( Impl&& )      = delete;
 
     void create_threads()
     {
-        abort_ = false;
-        threads_ = std::make_unique<std::thread[]>(thread_count_);// NOLINT
-        for (unsigned i = 0; i < thread_count_; ++i) { threads_[i] = std::thread(&Impl::worker, this); }
+        abort_   = false;
+        threads_ = std::make_unique< std::thread[] >( thread_count_ ); // NOLINT
+        for ( unsigned i = 0; i < thread_count_; ++i )
+        {
+            threads_[i] = std::thread( &Impl::worker, this );
+        }
     }
 
     void destroy_threads()
     {
         {
-            std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
+            std::unique_lock< std::mutex > tasks_lock( tasks_mutex_ );
             abort_ = true;
             task_added_.notify_all();
         }
-        for (unsigned i = 0; i < thread_count_; ++i) {
-            if (threads_[i].joinable()) { threads_[i].join(); }
+        for ( unsigned i = 0; i < thread_count_; ++i )
+        {
+            if ( threads_[i].joinable() )
+            {
+                threads_[i].join();
+            }
         }
         threads_.reset();
         thread_count_ = 0;
     }
 
-    static unsigned determine_thread_count(const unsigned thread_count)
+    static unsigned determine_thread_count( const unsigned thread_count )
     {
-        if (thread_count > 0) {
+        // we need at least two threads to process work and check futures
+        if ( thread_count > 0 )
+        {
             return thread_count;
-        } else {
-            if (std::thread::hardware_concurrency() > 0) {
+        }
+        else
+        {
+            if ( std::thread::hardware_concurrency() > 0 )
+            {
                 return std::thread::hardware_concurrency();
-            } else {
+            }
+            else
+            {
                 return 1;
             }
         }
     }
 
-    void add_task(task_proxy &&task)
+    void add_task( task_proxy&& proxy )
     {
+        if ( proxy.check_task( proxy.storage.get() ) )
         {
-            std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
-            tasks_.push(std::move(task));
+            {
+                std::unique_lock< std::mutex > lock( tasks_mutex_ );
+                tasks_.push( std::move( proxy ) );
+            }
+            ++tasks_queued_;
         }
-        ++tasks_queued_;
+        else
+        {
+            std::unique_lock< std::mutex > lock( check_tasks_mutex_ );
+            tasks_to_check_.push_back( std::move( proxy ) );
+        }
         task_added_.notify_one();
     }
 
@@ -84,13 +109,13 @@ struct task_pool::Impl
 
     std::size_t get_tasks_running() const
     {
-        std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
+        std::unique_lock< std::mutex > tasks_lock( tasks_mutex_ );
         return tasks_queued_ - tasks_.size();
     }
 
     std::size_t get_tasks_total() const
     {
-        std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
+        std::unique_lock< std::mutex > tasks_lock( tasks_mutex_ );
         return tasks_.size();
     }
 
@@ -100,14 +125,14 @@ struct task_pool::Impl
 
     void pause() { paused_ = true; }
 
-    void reset(const unsigned thread_count)
+    void reset( const unsigned thread_count )
     {
         const bool was_paused = paused_;
-        paused_ = true;
+        paused_               = true;
         wait_for_tasks();
         destroy_threads();
-        thread_count_ = determine_thread_count(thread_count);
-        paused_ = was_paused;
+        thread_count_ = determine_thread_count( thread_count );
+        paused_       = was_paused;
         create_threads();
     }
 
@@ -116,35 +141,107 @@ struct task_pool::Impl
     void wait_for_tasks()
     {
         waiting_ = true;
-        std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
+        std::unique_lock< std::mutex > tasks_lock( tasks_mutex_ );
         task_added_.notify_all();
-        task_completed_.wait(tasks_lock, [this] { return (tasks_queued_ == (paused_ ? tasks_.size() : 0)); });
+        task_completed_.wait(
+            tasks_lock, [this] { return ( tasks_queued_ == ( paused_ ? tasks_.size() : 0 ) ); } );
         waiting_ = false;
     }
 
     void cooperative_abort() { destroy_threads(); }
 
-    std::atomic<bool> const &get_stop_token() const { return abort_; }
+    std::atomic< bool > const& get_stop_token() const { return abort_; }
 
+    std::vector< task_proxy > checker() // must run with waiting_task_mutex held
+    {
+        if ( abort_ )
+        {
+            return {};
+        }
+        std::vector< task_proxy > ready_tasks;
+        {
+            auto const start   = std::begin( tasks_to_check_ );
+            auto const stop    = std::end( tasks_to_check_ );
+            auto const removed = std::remove_if( start, stop, []( auto const& proxy ) {
+                return proxy.check_task( proxy.storage.get() );
+            } );
+            if ( removed != stop )
+            {
+                ready_tasks.reserve(
+                    static_cast< std::size_t >( std::distance( removed, tasks_to_check_.end() ) ) );
+                std::copy( std::make_move_iterator( removed ),
+                           std::make_move_iterator( stop ),
+                           std::back_inserter( ready_tasks ) );
+            }
+            tasks_to_check_.erase( removed, stop );
+        }
+        return ready_tasks;
+    }
+
+    /**
+     * @brief Worker thread task
+     *
+     * @details All threads run the worker function. Tasks are allowed to take futures as arguments
+     * and instead of blocking an entire thread checking futures, something that can quickly fill up
+     * a thread pool we absorb the future checking into a argument wrapper for the task function.
+     * Then in the worker function we utilize unused threads to check the status of these futures
+     * and when tasks are ready to execute we schedule them.
+     *
+     * The idea would be that there is always some thread waiting on the tasks_mutex_ so before we
+     * take it we try to take the checker mutex and if we succeed we check all tasks that depend on
+     * futures. Once we have checked the futures we wake up any waiting thread to be the next
+     * checker before we take the tasks mutex.
+     */
     void worker()
     {
-        for (;;) {
-            std::unique_lock<std::mutex> tasks_lock(tasks_mutex_);
-            if (abort_) { break; };
-            task_added_.wait(tasks_lock, [this] { return !tasks_.empty() || abort_; });
-            if (!abort_ && !paused_) {
-                task_proxy proxy = std::move(tasks_.front());
-                tasks_.pop();
-                tasks_lock.unlock();
-                proxy.execute_task(proxy.storage.get());
-                --tasks_queued_;
-                if (waiting_) { task_completed_.notify_one(); }
+        for ( ;; )
+        {
+            {
+                std::vector< task_proxy > ready_tasks;
+                // workers first try to become the checker
+                std::unique_lock< std::mutex > checks_lock( check_tasks_mutex_, std::try_to_lock );
+                if ( checks_lock.owns_lock() && !tasks_to_check_.empty() )
+                {
+                    ready_tasks = checker();
+                    task_added_.notify_one(); // wake up the next checker
+                }
+                for ( auto& proxy : ready_tasks )
+                {
+                    add_task( std::move( proxy ) );
+                }
+            }
+            std::unique_lock< std::mutex > tasks_lock( tasks_mutex_ );
+            if ( abort_ )
+            {
+                break;
+            };
+            task_added_.wait( tasks_lock, [this] { return !tasks_.empty() || abort_; } );
+            if ( paused_ || tasks_.empty() )
+            {
+                // we where woken for abort or to be the next checker
+                continue;
+            }
+            if ( abort_ )
+            {
+                return;
+            }
+            task_proxy proxy = std::move( tasks_.front() );
+            tasks_.pop();
+            tasks_lock.unlock();
+            proxy.execute_task( proxy.storage.get() );
+            --tasks_queued_;
+            if ( waiting_ )
+            {
+                task_completed_.notify_one();
             }
         }
     }
 };
 
-task_pool::task_pool(const unsigned thread_count) : impl_{ new Impl(thread_count) } {}
+task_pool::task_pool( const unsigned thread_count )
+    : impl_{ new Impl( thread_count ) }
+{
+}
 
 task_pool::~task_pool()
 {
@@ -152,52 +249,95 @@ task_pool::~task_pool()
     // if (impl_ != nullptr) { impl_->cooperative_abort(); }
 }
 
-task_pool::task_pool(task_pool &&other) noexcept : impl_(std::move(other.impl_)) { other.impl_.reset(); }
+task_pool::task_pool( task_pool&& other ) noexcept
+    : impl_( std::move( other.impl_ ) )
+{
+    other.impl_.reset();
+}
 
-task_pool &task_pool::operator=(task_pool &&other) noexcept
+task_pool& task_pool::operator=( task_pool&& other ) noexcept
 {
     wait_for_tasks();
     impl_.reset();
-    std::swap(other.impl_, impl_);
+    std::swap( other.impl_, impl_ );
     return *this;
 }
 
-std::size_t task_pool::get_tasks_queued() const { return impl_->get_tasks_queued(); }
-
-std::size_t task_pool::get_tasks_running() const { return impl_->get_tasks_running(); }
-
-std::size_t task_pool::get_tasks_total() const { return impl_->get_tasks_total(); }
-
-unsigned task_pool::get_thread_count() const { return impl_->get_thread_count(); }
-
-bool task_pool::is_paused() const { return impl_->is_paused(); }
-
-void task_pool::pause() { impl_->pause(); }
-
-void task_pool::reset(const unsigned thread_count_) { impl_->reset(thread_count_); }
-
-void task_pool::unpause() { impl_->unpause(); }
-
-void task_pool::wait_for_tasks() { impl_->wait_for_tasks(); }
-
-stop_token task_pool::get_stop_token() const { return stop_token{ impl_->get_stop_token() }; }
-
-void task_pool::push_task(task_proxy &&task) { impl_->add_task(std::move(task)); }
-
-task_pool::task_proxy::task_proxy(task_proxy &&other) noexcept
-    : execute_task(other.execute_task), storage(std::move(other.storage))
+std::size_t task_pool::get_tasks_queued() const
 {
-    other.execute_task = [](void * /*unused*/) {};
+    return impl_->get_tasks_queued();
 }
 
-void task_pool::abort() { impl_->cooperative_abort(); }
-
-task_pool::task_proxy &task_pool::task_proxy::operator=(task_proxy &&other) noexcept
+std::size_t task_pool::get_tasks_running() const
 {
-    execute_task = other.execute_task;
-    storage = std::move(other.storage);
-    other.execute_task = [](void * /*unused*/) {};
+    return impl_->get_tasks_running();
+}
+
+std::size_t task_pool::get_tasks_total() const
+{
+    return impl_->get_tasks_total();
+}
+
+unsigned task_pool::get_thread_count() const
+{
+    return impl_->get_thread_count();
+}
+
+bool task_pool::is_paused() const
+{
+    return impl_->is_paused();
+}
+
+void task_pool::pause()
+{
+    impl_->pause();
+}
+
+void task_pool::reset( const unsigned thread_count_ )
+{
+    impl_->reset( thread_count_ );
+}
+
+void task_pool::unpause()
+{
+    impl_->unpause();
+}
+
+void task_pool::wait_for_tasks()
+{
+    impl_->wait_for_tasks();
+}
+
+stop_token task_pool::get_stop_token() const
+{
+    return stop_token{ impl_->get_stop_token() };
+}
+
+void task_pool::push_task( task_proxy&& task )
+{
+    impl_->add_task( std::move( task ) );
+}
+
+task_pool::task_proxy::task_proxy( task_proxy&& other ) noexcept
+    : check_task( other.check_task )
+    , execute_task( other.execute_task )
+    , storage( std::move( other.storage ) )
+{
+    other.execute_task = []( void* /*unused*/ ) {};
+}
+
+void task_pool::abort()
+{
+    impl_->cooperative_abort();
+}
+
+task_pool::task_proxy& task_pool::task_proxy::operator=( task_proxy&& other ) noexcept
+{
+    check_task         = other.check_task;
+    execute_task       = other.execute_task;
+    storage            = std::move( other.storage );
+    other.execute_task = []( void* /*unused*/ ) {};
     return *this;
 }
 
-}// namespace be
+} // namespace be
--- src/trait_fallbacks.h
+++ src/trait_fallbacks.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <chrono>
+#include <future>
+#include <mutex>
 #include <type_traits>
 #include <utility>
 
@@ -12,124 +15,183 @@
 #endif
 // std::invoke_result_t from cppreference.com
 namespace detail {
-template<class T> struct is_reference_wrapper : std::false_type
-{
-};
-template<class U> struct is_reference_wrapper<std::reference_wrapper<U>> : std::true_type
-{
-};
+template <class T> struct is_reference_wrapper : std::false_type {};
+template <class U>
+struct is_reference_wrapper<std::reference_wrapper<U>> : std::true_type {};
 
-template<class T> struct invoke_impl
-{
-    template<class F, class... Args>
-    static auto call(F &&f, Args &&...args) -> decltype(std::forward<F>(f)(std::forward<Args>(args)...));
+template <class T> struct invoke_impl {
+  template <class F, class... Args>
+  static auto call(F &&f, Args &&...args)
+      -> decltype(std::forward<F>(f)(std::forward<Args>(args)...));
 };
 
-template<class B, class MT> struct invoke_impl<MT B::*>
-{
-    template<class T,
-        class Td = typename std::decay<T>::type,
-        class = typename std::enable_if<std::is_base_of<B, Td>::value>::type>
-    static auto get(T &&t) -> T &&;
+template <class B, class MT> struct invoke_impl<MT B::*> {
+  template <
+      class T, class Td = typename std::decay<T>::type,
+      class = typename std::enable_if<std::is_base_of<B, Td>::value>::type>
+  static auto get(T &&t) -> T &&;
 
-    template<class T,
-        class Td = typename std::decay<T>::type,
-        class = typename std::enable_if<is_reference_wrapper<Td>::value>::type>
-    static auto get(T &&t) -> decltype(t.get());
+  template <
+      class T, class Td = typename std::decay<T>::type,
+      class = typename std::enable_if<is_reference_wrapper<Td>::value>::type>
+  static auto get(T &&t) -> decltype(t.get());
 
-    template<class T,
-        class Td = typename std::decay<T>::type,
-        class = typename std::enable_if<!std::is_base_of<B, Td>::value>::type,
-        class = typename std::enable_if<!is_reference_wrapper<Td>::value>::type>
-    static auto get(T &&t) -> decltype(*std::forward<T>(t));
+  template <
+      class T, class Td = typename std::decay<T>::type,
+      class = typename std::enable_if<!std::is_base_of<B, Td>::value>::type,
+      class = typename std::enable_if<!is_reference_wrapper<Td>::value>::type>
+  static auto get(T &&t) -> decltype(*std::forward<T>(t));
 
-    template<class T, class... Args, class MT1, class = typename std::enable_if<std::is_function<MT1>::value>::type>
-    static auto call(MT1 B::*pmf, T &&t, Args &&...args)
-        -> decltype((invoke_impl::get(std::forward<T>(t)).*pmf)(std::forward<Args>(args)...));
+  template <class T, class... Args, class MT1,
+            class = typename std::enable_if<std::is_function<MT1>::value>::type>
+  static auto call(MT1 B::*pmf, T &&t, Args &&...args)
+      -> decltype((invoke_impl::get(std::forward<T>(t)).*
+                   pmf)(std::forward<Args>(args)...));
 
-    template<class T> static auto call(MT B::*pmd, T &&t) -> decltype(invoke_impl::get(std::forward<T>(t)).*pmd);
+  template <class T>
+  static auto call(MT B::*pmd, T &&t)
+      -> decltype(invoke_impl::get(std::forward<T>(t)).*pmd);
 };
 
-template<class F, class... Args, class Fd = typename std::decay<F>::type>
-auto INVOKE(F &&f, Args &&...args) -> decltype(invoke_impl<Fd>::call(std::forward<F>(f), std::forward<Args>(args)...));
+template <class F, class... Args, class Fd = typename std::decay<F>::type>
+auto INVOKE(F &&f, Args &&...args)
+    -> decltype(invoke_impl<Fd>::call(std::forward<F>(f),
+                                      std::forward<Args>(args)...));
 
 // Conforming C++14 implementation (is also a valid C++11 implementation):
-template<typename AlwaysVoid, typename, typename...> struct invoke_result
-{
-};
-template<typename F, typename... Args>
-struct invoke_result<decltype(void(detail::INVOKE(std::declval<F>(), std::declval<Args>()...))), F, Args...>
-{
-    using type = decltype(detail::INVOKE(std::declval<F>(), std::declval<Args>()...));
+template <typename AlwaysVoid, typename, typename...> struct invoke_result {};
+template <typename F, typename... Args>
+struct invoke_result<decltype(void(detail::INVOKE(std::declval<F>(),
+                                                  std::declval<Args>()...))),
+                     F, Args...> {
+  using type =
+      decltype(detail::INVOKE(std::declval<F>(), std::declval<Args>()...));
 };
 
-}// namespace detail
+} // namespace detail
 #else
 #define BE_NODISGARD [[nodisguard]]
 #endif
 
-
 namespace be {
 
 #if __cplusplus < 201700
-template<class> struct result_of;
-template<class F, class... ArgTypes> struct result_of<F(ArgTypes...)> : detail::invoke_result<void, F, ArgTypes...>
-{
+template <class> struct result_of;
+template <class F, class... ArgTypes>
+struct result_of<F(ArgTypes...)> : detail::invoke_result<void, F, ArgTypes...> {
 };
-template<class F, class... ArgTypes> struct invoke_result : detail::invoke_result<void, F, ArgTypes...>
-{
-};
-template<typename Fn, typename... Args> using be_invoke_result_t = typename invoke_result<Fn, Args...>::type;
-template<class T> struct be_is_void : std::is_same<void, typename std::remove_cv<T>::type>
-{
-};
-template<typename T> constexpr bool be_is_void_v = be_is_void<T>::value;
+template <class F, class... ArgTypes>
+struct invoke_result : detail::invoke_result<void, F, ArgTypes...> {};
+template <typename Fn, typename... Args>
+using be_invoke_result_t = typename invoke_result<Fn, Args...>::type;
+template <class T>
+struct be_is_void : std::is_same<void, typename std::remove_cv<T>::type> {};
+template <typename T> constexpr bool be_is_void_v = be_is_void<T>::value;
+
+template <typename...> using be_void_t = void;
+
 #else
-template<typename Fn, typename... Args> using be_invoke_result_t = std::invoke_result_t<Fn, Args...>;
-template<typename T> constexpr bool be_is_void_v = std::is_void_v<T>;
+template <typename Fn, typename... Args>
+using be_invoke_result_t = std::invoke_result_t<Fn, Args...>;
+template <typename T> constexpr bool be_is_void_v = std::is_void_v<T>;
+
+template <typename... Ts> using be_void_t = void_t<Ts...>;
+
 #endif
+
+// Thank you Walter
+template <typename T, typename... Ts> struct is_one_of;
+template <typename T> struct is_one_of<T> : std::false_type {};
+template <typename T, typename... Ts>
+struct is_one_of<T, T, Ts...> : std::true_type {};
+template <typename T, typename U, typename... Ts>
+struct is_one_of<T, U, Ts...> : is_one_of<T, Ts...> {};
 
 struct stop_token;
 
-template<typename Tuple> struct contains_stop_token;
+template <typename... Ts>
+struct contains_stop_token : is_one_of<stop_token, Ts...> {};
 
-template<typename Tuple> struct contains_stop_token;
+template <typename T,
+          typename Functor = std::remove_reference_t<std::remove_cv_t<T>>>
+struct wants_stop_token
+    : public wants_stop_token<decltype(&Functor::operator())> {};
 
-template<template<typename...> class Tuple> struct contains_stop_token<Tuple<>> : public std::false_type
-{
+template <typename R, typename... Args>
+struct wants_stop_token<R(Args...)> : public contains_stop_token<Args...> {};
+
+template <typename R, typename... Args>
+struct wants_stop_token<R (*)(Args...)> : public contains_stop_token<Args...> {
 };
 
-template<template<typename...> class Tuple, typename... Args>
-struct contains_stop_token<Tuple<Args...>>
-    : public std::is_same<stop_token, std::decay_t<std::tuple_element_t<sizeof...(Args) - 1, std::tuple<Args...>>>>
-{
+template <class C, typename R, typename... Args>
+struct wants_stop_token<R (C::*)(Args...)>
+    : public contains_stop_token<Args...> {};
+
+template <class C, typename R, typename... Args>
+struct wants_stop_token<R (C::*)(Args...) const>
+    : public contains_stop_token<Args...> {};
+
+template <typename T>
+static constexpr bool wants_stop_token_v = wants_stop_token<T>::value;
+
+namespace future_api {
+template <typename Future>
+using get_result_t = decltype(std::declval<Future>().get());
+
+template <typename Future>
+using wait_result_t = decltype(std::declval<Future>().wait());
+
+template <typename Future>
+using wait_for_result_t = decltype(std::declval<Future>().wait_for(
+    std::declval<std::chrono::seconds>()));
+
+template <typename Future>
+using wait_until_result_t = decltype(std::declval<Future>().wait_until(
+    std::declval<std::chrono::steady_clock::time_point>()));
+
+template <typename T, typename = void> struct is_supported : std::false_type {};
+
+template <typename T>
+struct is_supported<T, be_void_t<get_result_t<T>, wait_result_t<T>,
+                                 wait_for_result_t<T>, wait_until_result_t<T>>>
+    : std::conditional<
+          std::is_same<void, wait_result_t<T>>::value &&
+              std::is_same<std::future_status, wait_for_result_t<T>>::value &&
+              std::is_same<std::future_status, wait_until_result_t<T>>::value,
+          std::true_type, std::false_type>::type {};
+
+} // namespace future_api
+
+template <typename T> struct is_future : future_api::is_supported<T>::type {};
+
+template <> struct is_future<double> : std::false_type {};
+
+template <typename... Ts> struct contains_future;
+template <typename T> struct contains_future<T> : is_future<T> {};
+template <typename T, typename... Ts>
+struct contains_future<T, Ts...>
+    : std::conditional_t<is_future<T>::value, std::true_type,
+                         contains_future<Ts...>> {};
+
+template <typename T> using future_value_t = future_api::get_result_t<T>;
+
+template <typename T, typename = void> struct future_argument {
+  using type = T;
 };
 
-template<typename T, typename Functor = std::remove_reference_t<std::remove_cv_t<T>>>
-struct wants_stop_token : public wants_stop_token<decltype(&Functor::operator())>
-{
+template <typename T>
+struct future_argument<T, be_void_t<future_api::get_result_t<T>>> {
+  using type = future_value_t<T>;
 };
 
-template<typename R, typename... Args>
-struct wants_stop_token<R(Args...)> : public contains_stop_token<std::tuple<Args...>>
-{
+template <typename T>
+using future_argument_t = typename future_argument<T>::type;
+
+template <typename T>
+struct is_movable : std::conditional_t<std::is_move_constructible<T>::value ||
+                                           std::is_move_assignable<T>::value,
+                                       std::true_type, std::false_type>::type {
 };
 
-template<typename R, typename... Args>
-struct wants_stop_token<R (*)(Args...)> : public contains_stop_token<std::tuple<Args...>>
-{
-};
-
-template<class C, typename R, typename... Args>
-struct wants_stop_token<R (C::*)(Args...)> : public contains_stop_token<std::tuple<Args...>>
-{
-};
-
-template<class C, typename R, typename... Args>
-struct wants_stop_token<R (C::*)(Args...) const> : public contains_stop_token<std::tuple<Args...>>
-{
-};
-
-template<typename T> static constexpr bool wants_stop_token_v = wants_stop_token<T>::value;
-
-}// namespace be
+} // namespace be
